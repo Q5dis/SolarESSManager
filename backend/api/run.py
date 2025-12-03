9d3ddc084a0196b3ff3541342d76eca8b751b000
taehyunan-99 <withblua@gmail.com>
2025-12-03 12:26:44 +0900

    2025-12-03 ATH: update backend

--- backend/api/run.py
+++ backend/api/run.py
@@ -1,12 +1,13 @@
 from app import create_app
 from app.services.relay_service import reset_relay
+import os
 
 app = create_app()
 
 if __name__=="__main__":
-    reset_relay()
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        reset_relay()
+
     app.run(host="0.0.0.0",
             port=5000,
-            debug=True,
-            use_reloader=False,
-            threaded = False)
+            debug=True)
